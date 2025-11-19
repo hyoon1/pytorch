587236f35024de3d8676b95f8db16d0f5bd1a419
Hosang Yoon <hosang.yoon@amd.com>
2025-11-19 13:25:59 -0500

    sdpa ck backend modification

--- aten/src/ATen/native/transformers/hip/flash_attn/ck/launch_kernel_pt.hpp
+++ aten/src/ATen/native/transformers/hip/flash_attn/ck/launch_kernel_pt.hpp
@@ -14,7 +14,7 @@ __launch_bounds__(MaxThreadPerBlock, MinBlockPerCu)
 #endif
     __global__ void kentry_pt(Args... args)
 {
-#if (defined(__gfx90a__) || defined(__gfx942__) || defined(__gfx950__) || defined(__gfx1201__))
+#if (defined(__gfx90a__) || defined(__gfx942__) || defined(__gfx950__) || defined(__gfx1200__) || defined(__gfx1201__))
     Kernel{}(args...);
 #else
     CUDA_KERNEL_ASSERT(false && "Fatal! Attempting to call a CK SDPA kernel on unsupported hardware");
